6b013388467698a4136d1711affff83303b8bdf5
Alexandru Rosu <alexandru.rosu98@gmail.com>
2025-03-14 20:49:23 +0200

    Culori

--- racheta/lansator/views.py
+++ racheta/lansator/views.py
@@ -7,3 +7,25 @@ def zile_pana_la_lansare(request):
 
 def nume_racheta_view(request):
     return HttpResponse("Numele rachetei A123")
+
+def racheta_template_view(request):
+    return render(request, "index.html")
+
+from datetime import datetime
+
+
+luni = ['Ianuarie', 'Februarie', 'Martie', 'Aprilie', 'Mai', 'Iunie', 'Iulie', 'August', 'Septembrie', 'Octombrie', 'Noiembrie', 'Decembrie']
+
+def today_view(request):
+   today = datetime.today()
+   year = today.year
+   month = today.month
+   day = today.day
+    
+
+
+   context = {
+       'data' : f"{day} - {luni [month - 1]} - {year}"
+   }
+   return render(request, "data.html", context )
+   return HttpResponse(today)
